f9187d4c99344e8352f72869387b71714ab41bd2
Utee <utibeabasiekong2@gmail.com>
2024-10-03 13:49:43 +0100

    feat: add test for the two functions written in the agro marketplace contract

---
+++ contracts/src/AgroMarketPlace.sol
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+import "../lib/openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
+import "../lib/openzeppelin-contracts/contracts/utils/Counters.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+
+contract AgroMarketPlace is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    struct MarketPlaceItem {
+        uint256 itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        string name;
+        uint256 price;
+        bool sold;
+    }
+
+    address payable owner;
+
+    mapping(uint256 => MarketPlaceItem) public idToMarketPlaceItem;
+
+    /**
+        Events
+     */
+    event MarketPlaceItemCreated(
+        uint256 indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        string name,
+        address seller,
+        uint256 price,
+        bool sold
+    );
+
+    event MarketPlaceItemSold(
+        uint256 indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        string name,
+        address seller,
+        uint256 price,
+        bool sold
+    );
+
+    // Listing a marketlace item
+    function listItemForSale(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price,
+        string memory name
+    ) public payable nonReentrant {
+        require(price > 0, "Price must be greater than 0");
+        IERC721 nft = IERC721(nftContract);
+        require(nft.ownerOf(tokenId) == msg.sender, "Caller is not owner");
+
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
+        idToMarketPlaceItem[itemId] = MarketPlaceItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            name,
+            price,
+            false
+        );
+
+        nft.transferFrom(msg.sender, address(this), tokenId);
+
+        emit MarketPlaceItemCreated(
+            itemId,
+            nftContract,
+            tokenId,
+            name,
+            msg.sender,
+            price,
+            false
+        );
+    }
+
+    // Querying the marketplace items
+    function purchaseItem(uint256 itemId) public payable nonReentrant {
+        MarketPlaceItem storage item = idToMarketPlaceItem[itemId];
+
+        require(
+            msg.value >= item.price,
+            "Not enough funds to purchase this product"
+        );
+        require(
+            msg.sender != item.seller,
+            "Seller cannot purchase their own product"
+        );
+
+        // Transfer Ether to the Seller
+
+        (bool success, ) = item.seller.call{value: item.price}("");
+        require(success, "Transfer to seller failed");
+
+        IERC721(item.nftContract).transferFrom(
+            address(this),
+            msg.sender,
+            item.tokenId
+        );
+
+        item.sold = true;
+
+        _itemsSold.increment();
+
+        emit MarketPlaceItemSold(
+            itemId,
+            item.nftContract,
+            item.tokenId,
+            item.name,
+            item.seller,
+            item.price,
+            true
+        );
+    }
+
+    // Todo: take into consideration the amount a user wants to purchase
+
+    // Purchase the marketplace items
+}
---
+++ contracts/test/AgroMarketPlace.t.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "forge-std/Test.sol";
+import "../src/AgroMarketPlace.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "./MockNFT.sol";
+
+contract AgroMarketPlaceTest is Test {
+    AgroMarketPlace agroMarketPlace;
+    MockNFT nft;
+
+    address owner;
+    address seller;
+    address buyer;
+
+    function setUp() public {
+        owner = address(this);
+        seller = address(0xBEEF);
+        buyer = address(0xCAFE);
+
+        agroMarketPlace = new AgroMarketPlace();
+        nft = new MockNFT();
+
+        // Mint an NFT to the seller
+        nft.mint(seller, 1);
+    }
+
+    function testListItemForSale() public {
+        vm.prank(seller);
+        nft.approve(address(agroMarketPlace), 1);
+        vm.prank(seller);
+        agroMarketPlace.listItemForSale(
+            address(nft),
+            1,
+            1 ether,
+            "Green Apple"
+        );
+    }
+
+    function testPurchaseItem() public {
+        testListItemForSale();
+
+        // purcahse item
+        vm.deal(buyer, 1 ether);
+        vm.prank(buyer);
+        agroMarketPlace.purchaseItem{value: 1 ether}(1);
+
+        (, , , , , , bool sold) = agroMarketPlace.idToMarketPlaceItem(1);
+        assertTrue(sold);
+    }
+
+    // Test passes because foundry passes a failed test prefixed with `testFail`
+    function testFailPurchaseItemWithoutEnoughFunds() public {
+        testListItemForSale();
+
+        // Buyer tries to purchase the item with insufficient funds
+        vm.deal(buyer, .5 ether);
+        vm.prank(buyer);
+        agroMarketPlace.purchaseItem{value: 0.5 ether}(1);
+    }
+
+    // Test passes because foundry passes a failed test prefixed with `testFail`
+    function testFailSellerCannotBuyOwnItem() public {
+        testListItemForSale();
+
+        vm.prank(seller);
+        agroMarketPlace.purchaseItem{value: 1 ether}(1);
+    }
+}
