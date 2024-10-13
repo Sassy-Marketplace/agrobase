c2606bc97728d67be70059f74437008b280356d7
Faith M. Roberts <faithroberts708@gmail.com>
2024-10-13 07:53:35 +0100

    added contract abi's

--- contracts/script/Counter.s.sol
+++ contracts/script/Counter.s.sol
@@ -7,18 +7,19 @@ import {AgroBaseProfile} from  "../src/AgroBaseProfileNFT.sol";
 import "../src/interfaces/IERC6551Registry.sol";
 import {IAccountProxy} from "../src/interfaces/IImplementation.sol";
 import "../src/CampaignFactory.sol";
-import {AgroMarketPlaceFactory} from "../src/AgroMarketPlace.sol";
+import {AgroMarketPlace} from "../src/AgroMarketPlace.sol";
 contract CounterScript is Script {
-    AgroBaseCore public agroBaseCore;
-    IERC6551Registry public mockRegistry;
-    IAccountProxy public mockAccountProxy;
-    AgroMarketPlaceFactory public marketplace;
-    CampaignFactory public campaign;
 
     function setUp() public {}
 
     function run() public {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privateKey);
+
+        AgroMarketPlace marketplace = new AgroMarketPlace(payable(0x04f6431098126Ded648f3C5589E2EF3beac09E15));
+        marketplace.owner();
+        CampaignFactory campaign = new CampaignFactory();
+        campaign.createCampaign("FEMA", 30, 1000, 1000, 0xE4aB69C077896252FAFBD49EFD26B5D171A32410, 0x4065400b4A4FEaE400aaa0e712E3d0e9F1AfeC87);
+        vm.stopBroadcast();
     }
 }
---
+++ dapp/src/utils/abis/contractAddresses.tsx
@@ -0,0 +1,3 @@
+export const coreAddress = 0x2fa49cb1de7d5a1f883a88ed4c0c09d68e384ba5;
+export const marketfactory = 0xaeb9efca10304d02a3dbc3d9028aca21e3c5639b;
+export const campaignfactory = 0x77af37d22022251ef600017149686308a56984e9;
